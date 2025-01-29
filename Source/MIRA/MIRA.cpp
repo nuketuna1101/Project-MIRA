8865ef1e9bf79a6e1d5589d1011fc8e6d70f0cb5
Jaeyoung <jyko1101@gmail.com>
2025-01-29 21:07:45 +0900

    feat: Logging setting
    MIRA 이름의 로깅 매크로 설정

--- Source/MIRA/MIRA.cpp
+++ Source/MIRA/MIRA.cpp
@@ -3,5 +3,6 @@
 #include "MIRA.h"
 #include "Modules/ModuleManager.h"
 
+DEFINE_LOG_CATEGORY(MIRA);
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MIRA, "MIRA" );
  
--- Source/MIRA/MIRA.h
+++ Source/MIRA/MIRA.h
@@ -1,5 +1,9 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #pragma once
+//#include "CoreMinimal.h"
 
-#include "CoreMinimal.h"
+// public macro settting for logging project MIRA
+#include "EngineMinimal.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(MIRA, Log, All);
