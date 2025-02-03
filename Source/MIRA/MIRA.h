29ff92491f531dfb104abb3c1f65efb01829849f
Jaeyoung <jyko1101@gmail.com>
2025-02-03 21:09:19 +0900

    feat: logging macro
    - MIRA 프로젝트용 로깅 매크로 추가

--- Source/MIRA/MIRA.h
+++ Source/MIRA/MIRA.h
@@ -7,3 +7,9 @@
 #include "EngineMinimal.h"
 
 DECLARE_LOG_CATEGORY_EXTERN(MIRA, Log, All);
+
+// Add Logging Macros
+#define MIRALOG_CALLINFO (FString(__FUNCTION__) + TEXT("(") + FString::FromInt(__LINE__) +TEXT(")"))
+#define MIRALOG_S(Verbosity) UE_LOG(MIRA, Verbosity, TEXT("%s"),*MIRALOG_CALLINFO)
+#define MIRALOG(Verbosity, Format,...) UE_LOG(MIRA, Verbosity, TEXT("%s%s"),*MIRALOG_CALLINFO, *FString::Printf(Format,##__VA_ARGS__))
+#define MIRACHECK(Expr, ...) {if(!(Expr)){MIRALOG(Error,TEXT("ASSERTION : %s"), TEXT("'"#Expr"'")); return __VA_ARGS__;}}
