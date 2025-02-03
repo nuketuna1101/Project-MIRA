bd151f8499d0c963ca90b4663d5354aafa23be27
Jaeyoung <jyko1101@gmail.com>
2025-02-03 21:09:47 +0900

    feat: PlayerController

---
+++ Source/MIRA/MIRAPlayerController.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MIRAPlayerController.h"
+
+void AMIRAPlayerController::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+}
+
+void AMIRAPlayerController::OnPossess(APawn* aPawn)
+{
+	Super::OnPossess(aPawn);
+}
---
+++ Source/MIRA/MIRAPlayerController.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "MIRA.h"
+#include "GameFramework/PlayerController.h"
+#include "MIRAPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MIRA_API AMIRAPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+	// components inits after instance created
+	virtual void PostInitializeComponents() override;
+	// use pawn by possessing
+	virtual void OnPossess(APawn* aPawn) override;
+
+};
