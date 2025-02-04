fd7042856be92d09fa995731742e04feffe72cf2
Jaeyoung <jyko1101@gmail.com>
2025-02-04 21:15:44 +0900

    feat: mira anim instance

---
+++ Source/MIRA/Private/MIRAAnimInstance.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MIRAAnimInstance.h"
+
+UMIRAAnimInstance::UMIRAAnimInstance()
+{
+	// pawn speed
+	CurrentPawnSpeed = 0.0f;
+}
---
+++ Source/MIRA/Public/MIRAAnimInstance.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "MIRA.h"
+#include "Animation/AnimInstance.h"
+#include "MIRAAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MIRA_API UMIRAAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+public:
+	UMIRAAnimInstance();
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Pawn", meta = (AllowPrivateAccess = true))
+	float CurrentPawnSpeed;
+	
+};
